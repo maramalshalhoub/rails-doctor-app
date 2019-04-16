2d23eedc56ff060728a69ff9fe78f752978eb551
Marc Wright <marc@generalassemb.ly>
2019-04-16 16:27:42 +0300

    full crud on appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,11 @@
 class AppointmentsController < ApplicationController
+  before_action :get_appointment, only: [:show, :edit, :destroy, :update]
+
   def index
     @appointments = Appointment.all
+  end
+
+  def show
   end
 
   def new
@@ -14,16 +19,27 @@ class AppointmentsController < ApplicationController
   end
 
   def edit
-    @appointment = Appointment.find(params[:id])
   end
 
   def update
-    
+    @appointment.update(appointment_params)
+
+    redirect_to @appointment
+  end
+
+  def destroy
+    @appointment.destroy
+
+    redirect_to appointments_path
   end
 
   private
 
     def appointment_params
       params.require(:appointment).permit(:doctor_id, :patient_id, :reason)
+    end
+
+    def get_appointment
+      @appointment = Appointment.find(params[:id])
     end
 end
