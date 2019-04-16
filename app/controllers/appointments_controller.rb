afb3a53248eddcae4e0a629cf26b2cedaba4ff0f
Marc Wright <marc@generalassemb.ly>
2019-04-16 14:56:35 +0300

    create new appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,4 +2,20 @@ class AppointmentsController < ApplicationController
   def index
     @appointments = Appointment.all
   end
+
+  def new
+    @appointment = Appointment.new    
+  end
+
+  def create
+    puts params
+    @appointment = Appointment.create(appointment_params)
+    redirect_to appointments_path
+  end
+
+  private
+
+    def appointment_params
+      params.require(:appointment).permit(:doctor_id, :patient_id, :reason)
+    end
 end
